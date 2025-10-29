cb010d24ec6dfc0b8bd648e6bdee620cd25c7f0a
Adiaxi <u56129@student.wat.edu.pl>
2025-10-29 11:12:28 +0100

    wip: added function to udpate user

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 users: list = [
-    {'name': 'Adrian', 'location': 'Wieluń', 'post': 4},
-    {'name': 'Patrycja', 'location': 'Warszawa', 'post': 26},
-    {'name': 'Kasia', 'location': 'Lublin', 'post': 12},
+    {'name': 'Adrian', 'location': 'Wieluń', 'posts': 4},
+    {'name': 'Patrycja', 'location': 'Warszawa', 'posts': 26},
+    {'name': 'Kasia', 'location': 'Lublin', 'posts': 12},
 ]
 
 
 def user_info(users_data: list) -> None:
     for user in users_data:
-        print(f'Twój znajomy {user['name']} w miejscowości {user['location']} opublikował {user["post"]} postów.')
+        print(f'Twój znajomy {user['name']} w miejscowości {user['location']} opublikował {user["posts"]} postów.')
 
-def add_user(users_data)->None:
+def add_user(users_data: list)->None:
     name:str=input('Podaj imię nowego znajomego: ')
     location:str=input('Podaj nazwę miejscowości: ')
     post:int=int(input('Podaj liczbę postów: '))
-    users_data.append({'name': name, 'location': location, 'post': post})
+    users_data.append({'name': name, 'location': location, 'posts': post})
 
 def remove_user(users_data: list)->None:
     temp_name:str= input('Podaj imię użytkownika do usunięcia z listy znajomych: ')
@@ -21,6 +21,13 @@ def remove_user(users_data: list)->None:
         if user['name'] == temp_name:
             users.remove(user)
 
+def update_user(users_data: list)->None:
+    temp_name:str= input('Podaj imię użytkownika do aktualizacji: ')
+    for user in users_data:
+        if user['name'] == temp_name:
+            user['name']=input('Podaj nowe imie: ')
+            user['location']=input('Podaj nową miejscowość: ')
+            user['posts']=input('Podaj nową ilość postów: ')
 
 while True:
     temp_choice: int = int(input('Wybierz opcję menu: '))
@@ -37,3 +44,4 @@ while True:
         remove_user(users)
     if temp_choice == 4:
         print('Wybrano funkcję aktualizacji danych znajomego')
+        update_user(users)
