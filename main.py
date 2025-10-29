418dc37aa3745aefdd0549c5d02885e6941a0c06
Adiaxi <u56129@student.wat.edu.pl>
2025-10-29 09:10:22 +0100

    wip: added for loop with users

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+zmienna_1:str= 'Adrian'
+zmienna_2:str= 'Michasia'
+zmienna_3:str= 'Kasia'
+zmienna_4:str= 'Asia'
+#przerob istniejacy kod w taki sposob aby zdefiniowani uzytkownicy
+uzytkownicy:list=[zmienna_1, zmienna_2, zmienna_3, zmienna_4]
+
+for uzytkownik in uzytkownicy:
+    print(f'Hello {uzytkownik}')
