1e712c41111637630024eff4760a576e30330ea6
Adiaxi <u56129@student.wat.edu.pl>
2025-11-19 13:07:06 +0100

    feat: added button functionality

--- main.py
+++ main.py
@@ -1,11 +1,122 @@
 from tkinter import *
 import os
 import tkinter
+
+from mapbook_lib.controller import update_user
+
 os.environ["TCL_LIBRARY"] = r"C:\Users\adria\AppData\Local\Programs\Python\Python313\tcl\tcl8.6"
 os.environ["TK_LIBRARY"]  = r"C:\Users\adria\AppData\Local\Programs\Python\Python313\tcl\tk8.6"
 
 import tkinter
 import tkintermapview
+
+
+users:list = []
+
+from bs4 import BeautifulSoup
+import requests
+
+
+class User:
+    def __init__(self,name:str,location:str,posts:int,img_url:str):
+        self.name = name
+        self.location = location
+        self.posts = posts
+        self.img_url = img_url
+        self.coords = self.get_coords()
+
+    def get_coords(self):
+        from bs4 import BeautifulSoup
+        import requests
+        url: str = f'https://pl.wikipedia.org/wiki/{self.location}'
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                          "(KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+        }
+        response = requests.get(url, headers=headers)
+
+        # print(response.text)
+        response_html = BeautifulSoup(response.text, "html.parser")
+        # print(response_html.prettify())
+
+        latitude = float(response_html.select('.latitude')[1].text.replace(',', '.'))
+        longitude = float(response_html.select('.longitude')[1].text.replace(',', '.'))
+        return ([latitude, longitude])
+
+def add_user(users_data: list)->None:
+    name:str=entry_name.get()
+    location:str=entry_lokalizacja.get()
+    post:int=int(entry_posty.get())
+    img_url:str=entry_img_url.get()
+    users_data.append(User(name=name,location=location,posts=post,img_url=img_url))
+    print(users_data)
+    user_info(users_data)
+
+    entry_name.delete(0, END)
+    entry_posty.delete(0, END)
+    entry_img_url.delete(0, END)
+    entry_lokalizacja.delete(0, END)
+    entry_name.focus()
+
+def user_info(users_data: list)-> None:
+    listbox_lista_obiektow.delete(0, END)
+    for idx, user in enumerate(users_data):
+        listbox_lista_obiektow.insert(END,f"{idx} {user.name} {user.location} {user.posts} {user.img_url} posty" )
+
+def delete_user(users_data: list)-> None:
+    i = listbox_lista_obiektow.index(ACTIVE)
+    users_data.pop(i)
+    user_info(users_data)
+
+def user_details(users_data: list)-> None:
+    i = listbox_lista_obiektow.index(ACTIVE)
+    label_imie_szczegoly_obiektu_wartosc.config(text=users_data[i].name)
+    label_lokalizacja_szczegoly_obiektu_wartosc.config(text=users_data[i].location)
+    label_posty_szczegoly_obiektu_wartosc.config(text=users_data[i].posts)
+
+def edit_user(users_data: list)-> None:
+    i = listbox_lista_obiektow.index(ACTIVE)
+    entry_name.insert(0, users_data[i].name)
+    entry_lokalizacja.insert(0, users_data[i].location)
+    entry_img_url.insert(0, users_data[i].img_url)
+    entry_posty.insert(0, users_data[i].posts)
+
+    button_dodaj_obiekt.config(text='Zapisz zmiany', command=lambda: update_user(users_data,i))
+
+def update_user(users_data: list, i) ->None:
+    users_data[i].name = entry_name.get()
+    users_data[i].location = entry_lokalizacja.get()
+    users_data[i].posts = entry_posty.get()
+    users_data[i].img_url = entry_img_url.get()
+    user_info(users_data)
+
+    button_dodaj_obiekt.config(text='Dodaj oobiekt', command=lambda: add_user(users_data))
+
+    entry_name.delete(0, END)
+    entry_posty.delete(0, END)
+    entry_img_url.delete(0, END)
+    entry_lokalizacja.delete(0, END)
+    entry_name.focus()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 root = Tk()
 root.title('mapbook')
@@ -29,13 +140,13 @@ label_lista_obiektow.grid(row=0, column=0, columnspan=3)
 listbox_lista_obiektow = Listbox(ramka_lista_obiektow)
 listbox_lista_obiektow.grid(row=1, column=0, columnspan=3)
 
-button_pokaz_szczegoly = Button(ramka_lista_obiektow, text='Pokaż szczegóły')
+button_pokaz_szczegoly = Button(ramka_lista_obiektow, text='Pokaż szczegóły', command=lambda: user_details(users))
 button_pokaz_szczegoly.grid(row=2, column=0)
 
-button_usun_obiekt = Button(ramka_lista_obiektow, text='Usuń obiekt')
+button_usun_obiekt = Button(ramka_lista_obiektow, text='Usuń obiekt', command = lambda: delete_user(users))
 button_usun_obiekt.grid(row=2, column=1)
 
-button_edytuj_obiekt = Button(ramka_lista_obiektow, text='Edytuj obiekt')
+button_edytuj_obiekt = Button(ramka_lista_obiektow, text='Edytuj obiekt', command = lambda: edit_user(users,))
 button_edytuj_obiekt.grid(row=2, column=2)
 
 
@@ -69,7 +180,7 @@ entry_posty.grid(row=3, column=1)
 entry_img_url = Entry(ramka_formularz)
 entry_img_url.grid(row=4, column=1)
 
-button_dodaj_obiekt = Button(ramka_formularz, text='Dodaj obiekt')
+button_dodaj_obiekt = Button(ramka_formularz, text='Dodaj obiekt', command=lambda: add_user(users))
 button_dodaj_obiekt.grid(row=5, column=0, columnspan=2)
 
 
@@ -78,7 +189,7 @@ button_dodaj_obiekt.grid(row=5, column=0, columnspan=2)
 
 # RAMKA SZCZEGÓŁY OBIEKTÓW
 label_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text='Szczegoly obiektu')
-label_szczegoly_obiektu.grid(row=6, column=0, sticky=W)
+label_szczegoly_obiektu.grid(row=0, column=0, sticky=W)
 
 label_imie_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text='Imie: ')
 label_imie_szczegoly_obiektu.grid(row=1, column=0)
